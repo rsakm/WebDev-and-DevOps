fcb1c1bb11610742ca277bb25643e80c87ae6d66
Rajshree <152796536+rsakm@users.noreply.github.com>
2025-03-19 06:40:00 +0530

    agrretaes to filter job stats

--- 8.4-Job Portal Backend/routes/jobsRoute.js
+++ 8.4-Job Portal Backend/routes/jobsRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import userAuth from '../middlewares/authMiddleware.js';
-import { createJobsController, deleteJobController, getJobsController, updateJobController } from '../controllers/jobsController.js';
+import { createJobsController, deleteJobController, getJobsController, jobStatsController, updateJobController } from '../controllers/jobsController.js';
 
 const router = express.Router();
 
@@ -16,5 +16,9 @@ router.patch('/update-job/:id',userAuth,updateJobController);
 
 // // Delete a job
 router.delete('/delete-job/:id',userAuth,deleteJobController);
+
+
+// // Job stats filter
+router.get('/job-stats',userAuth,jobStatsController);
 
 export default router;
