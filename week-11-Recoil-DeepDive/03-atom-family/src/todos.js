9e51e429b14690f1219cb48c3bd9cc875b4e730a
Rajshree <rajshreeakm@gmail.com>
2025-04-22 03:49:41 +0530

    Atom-Family

---
+++ week-11-Recoil-DeepDive/03-atom-family/src/todos.js
@@ -0,0 +1,9 @@
+export const TODOS = [{
+    id: 1,
+    title: "Go to Gym",
+    description: "Hit the gym from 7-9"
+}, {
+    id: 2,
+    title: "Practice DSA ",
+    description: "Solve DSA problems from from 9-11"
+},]
