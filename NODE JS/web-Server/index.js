1cd586b65ead60b5455d8b55d17e6e02fef9f1f0
Rajshree <rajshreeakm@gmail.com>
2025-05-13 22:16:02 +0530

    HTTP Server using node js

---
+++ NODE JS/web-Server/index.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+
+    const log = `${Date.now()}: ${req.method} ${req.url}`;
+
+    // if (req.url === '/') {
+        // res.write('Hello World');
+        // res.end();
+    // }
+    
+fs.appendFile('log.txt', log + '\n', (err) => {
+    switch(req.url){
+        case '/':
+            res.write('Home page');
+            res.end();
+            break;
+        case '/about':
+            res.write('About Page');
+            res.end();
+            break;
+        default:
+            res.write('404');
+            res.end();
+            break; 
+    }
+})
+    
+});
+
+server.listen(3000,()=>{
+    console.log("Listening on port 3000");
+});
